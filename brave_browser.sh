c2890d58980211649226a85faef1d0f08e009b8f
inhibitor128 <76563632+inhibitor128@users.noreply.github.com>
2021-06-19 15:12:57 -0400

    Update brave_browser.sh
    added read message

--- brave_browser.sh
+++ brave_browser.sh
@@ -8,19 +8,19 @@
 
 
 echo "########## Installing Brave Browser for Ubuntu ##########"
-
+read -p "Press enter to continue"
 echo "########## Allowing the use of repositories accessed via the HTTP Secure protocol ##########"
 sudo apt install apt-transport-https curl
-
+read -p "Press enter to continue"
 echo "########## Installing security key ##########"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
+read -p "Press enter to continue"
 echo "########## Instaling brave repository ##########"
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
+read -p "Press enter to continue"
 echo "########## System update ##########"
 sudo apt update
-
+read -p "Press enter to continue"
 echo "########## Install Brave Browser ##########"
 sudo apt install brave-browser
 
