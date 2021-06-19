57cb3e8771fe32f512b2c09d3edbb14c6ca41ef5
inhibitor128 <76563632+inhibitor128@users.noreply.github.com>
2021-06-19 11:55:10 -0400

    Update rm_brave.sh

--- rm_brave.sh
+++ rm_brave.sh
@@ -4,6 +4,6 @@ sudo apt remove brave-browser
 
 sudo apt purge brave-browser
 
-rm -rf ~/.config/BraveSoftware/Brave-Browser/
-
 sudo rm /etc/apt/sources.list.d/brave-browser-release.list
+
+rm -rf ~/.config/BraveSoftware/Brave-Browser/
