b9b4d888b71427a5fe411c9500c32e28e2c49535
inhibitor128 <76563632+inhibitor128@users.noreply.github.com>
2021-06-26 10:04:59 -0400

    Update coinFlip.sh

--- coinFlip.sh
+++ coinFlip.sh
@@ -9,3 +9,10 @@ if [ $rand -gt 5 ];
 	else
 	echo "Tails"
 fi
+
+#num1 -eq num2 check if 1st  number is equal to 2nd number
+#num1 -ge num2 checks if 1st  number  is greater than or equal to 2nd number
+#num1 -gt num2 checks if 1st  number is greater than 2nd number
+#num1 -le num2 checks if 1st number is less than or equal to 2nd number
+#num1 -lt num2 checks if 1st  number  is less than 2nd number
+#num1 -ne num2 checks if 1st  number  is not equal to 2nd number
